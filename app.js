68ac1ed8f981d86d3102f032304e737c4e2956d0
mchen1235 <michaelschen123@gmail.com>
2021-04-25 17:31:09 -0700

    Added a ./fishes route

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var exphbs  = require('express-handlebars');
 var app     = express();
 PORT        = 19524;
 
+//temp data
+fishes_data = require('./temp_fish_data.json');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
 app.get('/', function(req, res){
     res.status(200).render('temp')
+});
+
+app.get('/fishes', function(req, res){
+	res.status(200).render('entity_table', fishes_data)
 });
 
 app.get('*', function(req, res){
